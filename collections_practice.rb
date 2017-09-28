69c64cc43d68d1a682b5787a7baf99341b69357a
Matt Hough <matt@hough.tv>
2017-09-28 00:05:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,7 +21,7 @@ end
 #  array
 #end
 
-def swap_elements(array, index = 1, destination_index = 2)
+def swap_elements_adv(array, index = 1, destination_index = 2)
   array[index], array[destination_index] = array[destination_index], array[index]
   array
 end
